6f6275112ea61f0c235286b402ee3a7f39ee12ee
Nattaporn  Pongpluem <pongpluem@hotmail.com>
2023-04-03 10:21:39 +0700

    first commit

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -11,17 +11,16 @@ const config = {
 
 export default async function handler(req, res) {
     try {
-        const message = req.body.message;
-        const event = req.body.event
+        const message = "";        
 
         console.log(process.env.CHANNEL_ACCESS_TOKEN)
 
         //console.log("req")
         //console.log(req)
-        console.log("body")
-        console.log(req.body)
-        console.log("message")
-        console.log(message)
+        //console.log("body")
+        //console.log(req.body)
+        //console.log("message")
+        //console.log(message)
         /*
         await client.pushMessage("", {
           type: "text",
@@ -29,7 +28,33 @@ export default async function handler(req, res) {
         });
         */
 
+        /*
         Promise.all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result))
+        .catch((err) => {
+          console.error(err);
+          res.status(500).end();
+        });
+        */
+
+        Promise.all(req.body.events.map(async (event)=>{
+            if (event.type !== 'message' || event.message.type !== 'text') {       
+                return Promise.resolve(null);
+              }
+            
+              message = event.message.text
+              const replyToken = event.replyToken
+              console.log("this is message")
+              console.log(event.message.text)
+          
+              console.log("this is token")
+              console.log(event.replyToken)
+
+            await client.pushMessage(replyToken, {
+                type: "text",
+                text: message,
+              });
+        }))
         .then((result) => res.json(result))
         .catch((err) => {
           console.error(err);
@@ -53,6 +78,8 @@ export default async function handler(req, res) {
 
     console.log("this is token")
     console.log(event.replyToken)
+
+    
 
     return client.replyMessage(event.replyToken, {
       type: 'text',
