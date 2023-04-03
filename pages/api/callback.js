bdceaf157bcd0101558c3cb0cc5e044ef717b08d
Nattaporn  Pongpluem <pongpluem@hotmail.com>
2023-04-03 09:57:14 +0700

    first commit

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -12,12 +12,13 @@ const config = {
 export default async function handler(req, res) {
     try {
         const message = req.body.message;
+        const event = req.body.event
 
         console.log(process.env.CHANNEL_ACCESS_TOKEN)
 
-        console.log("req")
-        console.log(req)
-        console.log("bofy")
+        //console.log("req")
+        //console.log(req)
+        console.log("body")
         console.log(req.body)
         console.log("message")
         console.log(message)
@@ -27,10 +28,29 @@ export default async function handler(req, res) {
           text: message,
         });
         */
+
+        Promise.all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result))
+        .catch((err) => {
+          console.error(err);
+          res.status(500).end();
+        });
+
         res.status(200).json({ message: `${message}Successs` });
       } catch (e) {
         res.status(500).json({ message: `error! ${e} ` });
       }
     
+  }
+
+  function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      return Promise.resolve(null);
+    }
+  
+    return client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: event.message.text,
+    });
   }
 
